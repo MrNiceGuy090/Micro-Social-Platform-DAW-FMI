70d0d56840e6cc69e0127890a94965296ff5ff94
cul1n <56848256+cul1n@users.noreply.github.com>
2020-12-08 16:51:42 +0200

    Added Friend Requests, Friendships

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -20,6 +20,18 @@ namespace MicroSocialPlatform.Controllers
 
             var posts = db.Posts.Where(p => p.UserId == profil.Id).OrderByDescending(p => p.Date).ToList();
             ViewBag.Posts = posts;
+
+
+
+            // Este prieten sau a primit request
+            if (db.Friendship.Find(id, User.Identity.GetUserId()) != null)
+                ViewBag.Friendship = "prieteni";
+            else if (db.Request.Find(User.Identity.GetUserId(),id) != null)
+                ViewBag.Friendship = "trimisa";
+            else if (db.Request.Find(id, User.Identity.GetUserId()) != null)
+                ViewBag.Friendship = "primita";
+            else if (id != User.Identity.GetUserId())
+                ViewBag.Friendship = "straini";
             return View();
         }
     }
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -30,6 +30,9 @@ namespace MicroSocialPlatform.Models
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
+        public DbSet<Friendship> Friendship { get; set; }
+        public DbSet<Request> Request { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
